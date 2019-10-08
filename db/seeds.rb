ee351582d1eac228da64ea3d56860a5c46430909
Bryce <brycebrown91@hotmail.com>
2019-10-08 15:33:40 -0500

    created Genres controller and started parsing csv data in seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Genre.destroy_all
+
+require 'csv'
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'book_data.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+csv.each do |row|
+  # convert to string first to bypass 'undefined method split for nil' error
+  puts row['genres'].to_s.split('|')
+  Genre.find_or_create_by(name: row['genres'].to_s.split('|'))
+end
+
+puts Genre.count
