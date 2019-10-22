d90efeeac0c326fb0d21ad0ece28a9d2a8256afd
Bryce <brycebrown91@hotmail.com>
2019-10-22 14:44:23 -0500

    Implemented simple search for book titles

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,8 @@ class AuthorsController < ApplicationController
   def index
     @authors = Author.all # This instance vaariable will be shared with the view
   end
+
+  def show
+    @author = Author.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
 
   resources :books, only: %i[index show]
 
+  get '/search', to: 'books#search'
+
   root to: 'books#index'
 end
